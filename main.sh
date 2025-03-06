61e7c9f1fc8218c989d909adc3df863c3402c8a4
farukyld <123uzunince123@gmail.com>
2025-03-06 10:42:10 +0300

    - uzun satırlar bölündü - mevcut dizini güncellemeyle ilgili bilgilendirme mesajı güncellendi

--- main.sh
+++ main.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-read -p "initramfs olarak ayarlanacak dizini girin (varsayilan: initramfs): " initramfs_dir
+read -p "initramfs olarak ayarlanacak dizini \
+girin (varsayilan: initramfs): " initramfs_dir
 initramfs_dir=${initramfs_dir:-initramfs}  # If empty, set default to "initramfs"
 
 
 initramfs_taslak_script="./initramfs_taslak_olustur.sh"
 
 if [ -d $initramfs_dir ]; then
-  read -p "$initramfs_dir dizini daha once olusturulmus. (potansiyel olarak) uzerine yazmak ister misiniz? (y/N): " secim
+  read -p "$initramfs_dir dizini daha once olusturulmus. uzerine yazmak \
+  ister misiniz (!$initramfs_dir'de yaptiginiz degisiklikleri \
+  kaybedebilirsiniz)? (y/N): " secim
 
   case "$secim" in
     [yY] )
@@ -28,9 +31,11 @@ initramfs_home_dir=$initramfs_dir/home/user1
 echo "$initramfs_home_dir dizini olusturuldu"
 
 mkdir -p $initramfs_home_dir
-echo "simulasyon sirasinda kullanmak istediginiz dosyalarin path'lerini giriniz."
+echo "simulasyon sirasinda kullanmak istediginiz dosyalarin path'lerini \
+giriniz."
 echo "bu dosyalar $initramfs_home_dir dizinine kopyalanacaktir."
-echo "calistirmak istediginiz programlar, riscv-unknown-linux-gnu-xxx -static olarak derlenmis olmalilar."
+echo "calistirmak istediginiz programlar, riscv-unknown-linux-gnu-xxx \
+-static olarak derlenmis olmalilar."
 echo "bu islemi su an manuel olarak yaptiysaniz bu adimi gecebilirsiniz."
 
 read -p " kopyalanacak dosyalar: " kopyalanacak_dosyalar
@@ -50,8 +55,10 @@ initramfs_boyut_byte=$(wc -c < $initramfs_dir.cpio)
 # gecmiyorsa hata mesaji verip bitir.
 dts_taslak=taslak.dts
 
-if ! grep -q "INITRD_START" "$dts_taslak" || ! grep -q "INITRD_END" "$dts_taslak"; then
-  echo "Hata: INITRD_START veya INITRD_END kelimeleri $dts_taslak dosyasi içinde bulunamadi."
+if ! grep -q "INITRD_START" "$dts_taslak" \
+|| ! grep -q "INITRD_END" "$dts_taslak"; then
+  echo "Hata: INITRD_START veya INITRD_END \
+  kelimeleri $dts_taslak dosyasi içinde bulunamadi."
   echo "taslak dts dosyanizda soyle bir bolum bulundugundan emin olun:"
   echo  "/dts-v1/;"
   echo  ""
@@ -61,7 +68,8 @@ if ! grep -q "INITRD_START" "$dts_taslak" || ! grep -q "INITRD_END" "$dts_taslak
   echo  "      stdout-path = &SERIAL0;" // farkli olabilir?
   echo  "      linux,initrd-start = <INITRD_START>;"
   echo  "      linux,initrd-end = <INITRD_END>;"
-  echo  "      bootargs = "root=/dev/ram rw console=ttyS0 earlycon=sbi";"// farkli olabilir?
+  echo  "      bootargs = "root=/dev/ram rw console=ttyS0 earlycon=sbi";"\
+  // farkli olabilir?
   echo  "  };"
   echo  "  // diger satirlar"
   echo  "  };"
@@ -104,7 +112,8 @@ make_cmd="make FW_DYNAMIC=n FW_JUMP=n FW_PAYLOAD=y \
 eval $make_cmd
 
 if [ $? -ne 0 ]; then
-  echo "opensbi build'leme basarisiz oldu. build dizini temizlenip yeniden deneniyor."
+  echo "opensbi build'leme basarisiz oldu. build dizini \
+  temizlenip yeniden deneniyor."
   rm -rf build/*
   eval $make_cmd
 fi
